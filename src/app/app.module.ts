4bd50e5acef651ee29c0a7ac1fcb531dff23434a
smac89 <smac89@users.noreply.github.com>
2017-05-07 16:06:36 -0600

    Added a bit of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {HeroService} from './hero.service';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        // The router matches this route's path to the URL in the browser address bar
+        path: 'heroes',
+        // The component that the router should create when navigating to this route
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [HeroService]
 })
 
 export class AppModule { }
